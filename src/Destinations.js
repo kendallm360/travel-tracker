516109de75cb2eb4c3586aecc907fa6ff9026498
Kendall McGree <mcgreekendall@gmail.com>
2022-06-07 20:12:15 -0500

    add tests for destinations' class properties, all pass

--- src/Destinations.js
+++ src/Destinations.js
@@ -1,3 +1,12 @@
 export default class Destinations {
-  constructor(destinationData) {}
+  constructor(destinationData) {
+    this.id = destinationData.id;
+    this.destination = destinationData.destination;
+    this.estimatedLodgingCostPerDay =
+      destinationData.estimatedLodgingCostPerDay;
+    this.estimatedFlightCostPerPerson =
+      destinationData.estimatedFlightCostPerPerson;
+    this.image = destinationData.image;
+    this.alt = destinationData.alt;
+  }
 }
--- test/Destinations-test.js
+++ test/Destinations-test.js
@@ -25,4 +25,32 @@ describe("Destinations", () => {
   it("Should be an instance of the Destination class", () => {
     expect(destination1).to.be.an.instanceOf(Destinations);
   });
+
+  it("Should have an id for the destination", () => {
+    expect(destination2.id).to.equal(2);
+  });
+
+  it("Should have destination", () => {
+    expect(destination3.destination).to.equal("Sydney, Austrailia");
+  });
+
+  it("Should have an estimated cost for lodging per day", () => {
+    expect(destination3.estimatedLodgingCostPerDay).to.equal(130);
+  });
+
+  it("Should have an estimated cost for the flight per person", () => {
+    expect(destination2.estimatedFlightCostPerPerson).to.equal(780);
+  });
+
+  it("Should have a link to an image of the destination", () => {
+    expect(destination1.image).to.equal(
+      "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80"
+    );
+  });
+
+  it("Should have alt text for the image of the destination", () => {
+    expect(destination1.alt).to.equal(
+      "overview of city buildings with a clear sky"
+    );
+  });
 });
